76a443dce47d02304222c2959619bb8b8398e4a9
jchristian1@sheffield.ac.uk <jeremychristian@Jeremys-MacBook-Pro.local>
2016-03-26 15:13:58 +0000

    Sends JSON

--- Assignment/routes/index.js
+++ Assignment/routes/index.js
@@ -6,4 +6,13 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/string', function(req, res, next) {
+
+	var text = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
+    var obj = JSON.parse(text);
+	
+  	res.send({ some: 'json' });
+
+});
+
 module.exports = router;
